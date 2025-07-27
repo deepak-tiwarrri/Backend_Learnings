0c098862472b6011022435f984380bc0cb91e168
DeepakPC <ainutiwari483@gmail.com>
2025-07-27 23:11:29 +0530

    handling method in nodejs

--- index.js
+++ index.js
@@ -2,13 +2,32 @@
 
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
+
 const server = http.createServer((req, res) => {
+  if (req.url === "/favicon.ico") return res.end();
   res.writeHead(200, "Hello, deepak");
+  const _URL = url.parse(req.url, true);
+  console.log("Full url", _URL);
+  // console.log(_URL.)
+  console.log("pahtname: ", _URL.pathname);
   let log = `${Date.now()}: ${req.url} New Request added\n`;
+
   fs.appendFile("./log.txt", log, () => {
-    console.log("append file");
+    switch (_URL.pathname) {
+      case "/":
+        res.end("Homepage");
+        break;
+      case "/about":
+        res.end(`You have searched: ${JSON.stringify(_URL.query)}`);
+        break;
+
+      default:
+        res.end("404 found page");
+        break;
+    }
+    console.log("Request logged to file");
   });
-  res.end("server is working");
 });
 
 const PORT = 8000;
