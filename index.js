75517cd252255972095fc040c188e3676c20d476
DeepakPC <ainutiwari483@gmail.com>
2025-07-27 20:55:45 +0530

    handling url in nodejs

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-// import { add, sub } from "./math.js";
-const { add, sub } = require("./math");
-// import math from "./math.js";
-console.log(add(2, 3));
-console.log(sub(3, 3));
+//start a server
+
+const http = require("http");
+const fs = require("fs");
+const server = http.createServer((req, res) => {
+  res.writeHead(200, "Hello, deepak");
+  let log = `${Date.now()}: ${req.url} New Request added\n`;
+  fs.appendFile("./log.txt", log, () => {
+    console.log("append file");
+  });
+  res.end("server is working");
+});
+
+const PORT = 8000;
+
+server.listen(PORT, () => {
+  console.log(`Server started on port: ${PORT}`);
+});
