3b16c4f2d88513497e440a29fc9f6e2171ab0a1d
DeepakPC <ainutiwari483@gmail.com>
2025-07-28 15:07:11 +0530

    moved files and started with expressjs

--- index.js
+++ index.js
@@ -1,37 +1,15 @@
-//start a server
-
-const http = require("http");
-const fs = require("fs");
-const url = require("url");
-
-const server = http.createServer((req, res) => {
-  if (req.url === "/favicon.ico") return res.end();
-  res.writeHead(200, "Hello, deepak");
-  const _URL = url.parse(req.url, true);
-  console.log("Full url", _URL);
-  // console.log(_URL.)
-  console.log("pahtname: ", _URL.pathname);
-  let log = `${Date.now()}: ${req.url} New Request added\n`;
-
-  fs.appendFile("./log.txt", log, () => {
-    switch (_URL.pathname) {
-      case "/":
-        res.end("Homepage");
-        break;
-      case "/about":
-        res.end(`You have searched: ${JSON.stringify(_URL.query)}`);
-        break;
-
-      default:
-        res.end("404 found page");
-        break;
-    }
-    console.log("Request logged to file");
-  });
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.send("This is Homepage");
 });
 
-const PORT = 8000;
-
-server.listen(PORT, () => {
-  console.log(`Server started on port: ${PORT}`);
+app.get("/about", (req, res) => {
+  res.send(`This is About page ${req.query.name}`);
+});
+app.get("/age", (req, res) => {
+  res.send(`Your age is ${req.query.age}`);
+});
+app.listen(8000, () => {
+  console.log(`Server started and listening...`);
 });
