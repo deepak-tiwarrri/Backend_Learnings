1648ca30d1208a3bc42998fddc10f592d3c39112
DeepakPC <ainutiwari483@gmail.com>
2025-07-29 15:16:30 +0530

    expressjs methods

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const express = require("express");
+const users = require("./MOCK_DATA.json");
+
 const app = express();
-app.get("/", (req, res) => {
-  res.send("This is Homepage");
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+//dynamic id
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.filter((item) => item.id === id);
+  return res.json(user);
 });
 
-app.get("/about", (req, res) => {
-  res.send(`This is About page ${req.query.name}`);
+app.get("/users", (req, res) => {
+  const html = `
+    <ul>
+       ${users.map((item) => `<li>${item.first_name}</li></br>`).join("")}
+    </ul>
+  `;
+  res.send(html);
 });
-app.get("/age", (req, res) => {
-  res.send(`Your age is ${req.query.age}`);
-});
-app.listen(8000, () => {
-  console.log(`Server started and listening...`);
+
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`Server started on ${PORT}`);
 });
